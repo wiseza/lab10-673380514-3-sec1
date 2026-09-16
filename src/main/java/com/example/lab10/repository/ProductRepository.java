771cb5b967ae19086e74cd7ae7d1450a09839bd4
wiseza <wiphawi_ri@kkumail.com>
2026-09-16 14:33:02 +0700

    Update ProductRepository.java

--- src/main/java/com/example/lab10/repository/ProductRepository.java
+++ src/main/java/com/example/lab10/repository/ProductRepository.java
@@ -7,6 +7,7 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+
 /**
  * ProductRepository — In-memory Reactive Repository
  *
@@ -46,9 +47,12 @@ public class ProductRepository {
      *       ถ้ามีค่าให้ใช้ Mono.just(product)
      */
     public Mono<Product> findById(String id) {
-        // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
-    }
+        Product product = store.get(id); 
+        if (product == null) { 
+            return Mono.empty(); 
+            } 
+            return Mono.just(product); 
+            }
 
     // ── 2. หา Product ทั้งหมด ────────────────────────────
     /**
@@ -58,8 +62,8 @@ public class ProductRepository {
      *       ใช้ Flux.fromIterable(...) แปลงเป็น Flux
      */
     public Flux<Product> findAll() {
-        // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+
+        return Flux.fromIterable(store.values()); // ← แก้บรรทัดนี้
     }
 
     // ── 3. บันทึก Product ────────────────────────────────
@@ -70,8 +74,8 @@ public class ProductRepository {
      *       แล้วใช้ Mono.just(product) คืนค่า
      */
     public Mono<Product> save(Product product) {
-        // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        store.put(product.getId(), product);
+        return Mono.just(product); // ← แก้บรรทัดนี้
     }
 
     // ── 4. ลบ Product ────────────────────────────────────
@@ -82,8 +86,8 @@ public class ProductRepository {
      *       แล้วใช้ Mono.empty() คืนค่า (Mono<Void>)
      */
     public Mono<Void> deleteById(String id) {
-        // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        store.remove(id);
+        return Mono.empty(); // ← แก้บรรทัดนี้
     }
 
     // ── 5. กรองตาม category ──────────────────────────────
@@ -94,7 +98,7 @@ public class ProductRepository {
      *       .filter(p -> p.getCategory().equalsIgnoreCase(category))
      */
     public Flux<Product> findByCategory(String category) {
-        // TODO: เติม code ตรงนี้
-        return null; // ← แก้บรรทัดนี้
+        return findAll()
+                .filter(p -> p.getCategory().equalsIgnoreCase(category));// ← แก้บรรทัดนี้
     }
 }
